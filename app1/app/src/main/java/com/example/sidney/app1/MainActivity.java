2edadb21e1856c6bbf5edad64dc96539d6659038
Sidney Lins <slinstj@gmail.com>
2018-05-14 00:31:54 -0300

    Play on code using TextView and EditView set/getText()

--- app1/app/src/main/java/com/example/sidney/app1/MainActivity.java
+++ app1/app/src/main/java/com/example/sidney/app1/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.sidney.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nomain);
+        setContentView(R.layout.activity_main);
+
+        TextView lblHello = (TextView)findViewById(R.id.lblHello);
+        String texto = lblHello.getText().toString();
+        lblHello.setText(texto + " mudado");
+
+        EditText txtNome = (EditText)findViewById(R.id.txtNome);
+        txtNome.setText(lblHello.getText().toString() + " - e de novo.");
     }
 }
